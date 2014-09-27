1cc9cd25d23377cc67e2afb96e7dcfe656adf7d5
Quentin Dufour <quentin@dufour.tk>
2014-09-27 14:58:07 +0200

    Front page

--- src/InsaLan/InformationBundle/Controller/DefaultController.php
+++ src/InsaLan/InformationBundle/Controller/DefaultController.php
@@ -9,13 +9,40 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/faq-normal")
      * @Template()
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
+        return array();
+    }
+    
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function homeAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/faq")
+     * @Template()
+     */
+    public function faqAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/rules")
+     * @Template()
+     */
+    public function rulesAction()
+    {
         return array();
     }
 }
