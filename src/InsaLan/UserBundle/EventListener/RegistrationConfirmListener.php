62806b295bf57f33f8a891b2c6c6b03f93073fff
Quentin Dufour <quentin@dufour.tk>
2014-09-27 20:43:06 +0200

    [USER] Redirect to profile after registration

---
+++ src/InsaLan/UserBundle/EventListener/RegistrationConfirmListener.php
@@ -0,0 +1,35 @@
+<?php
+namespace InsaLan\UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\UserEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class RegistrationConfirmListener implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+                FOSUserEvents::REGISTRATION_CONFIRM => 'onRegistrationConfirm'
+        );
+    }
+
+    public function onRegistrationConfirm($event)
+    {
+        $url = $this->router->generate('fos_user_profile_show');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
