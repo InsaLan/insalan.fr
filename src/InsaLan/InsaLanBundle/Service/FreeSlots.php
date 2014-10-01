32941bbb9382158311d48ab3553794dca3c1925e
Lesterpig <contact@lesterpig.com>
2014-10-01 10:52:38 +0200

    Show correct number of free slots
    Probably not stable. Needs code review.
    I used twig globals and service container.
    -> 1 SQL request by page. Improve this with a tiny cache ?

---
+++ src/InsaLan/InsaLanBundle/Service/FreeSlots.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * It probably needs code review.
+ *
+ * Affects app/config/config.yml with a twig global variable.
+ *
+ * @author Lesterpig
+ */
+
+namespace InsaLan\InsaLanBundle\Service;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+class FreeSlots
+{ 
+    const TOTAL_SLOTS = 64;
+
+    protected $doctrine;
+
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function get()
+    {
+       return self::TOTAL_SLOTS - count($this->doctrine
+       	                                     ->getRepository('InsaLanTournamentBundle:Participant')
+                                             ->findByValidated(true));
+    }
+}
+?>
