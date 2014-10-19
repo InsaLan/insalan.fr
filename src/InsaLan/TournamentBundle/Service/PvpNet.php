c3adff03221ee1dd3b3faba115ad8ec734fe91f2
Lesterpig <contact@lesterpig.com>
2014-10-19 18:22:31 +0200

    Added checkGame service
    It seems to work properly :)

--- src/InsaLan/TournamentBundle/Service/PvpNet.php
+++ src/InsaLan/TournamentBundle/Service/PvpNet.php
@@ -3,9 +3,67 @@
 namespace InsaLan\TournamentBundle\Service;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use InsaLan\InsaLanBundle\API\Lol;
+use InsaLan\TournamentBundle\Entity\Team;
 
 class PvpNet
 { 
+
+    private $API;
+
+    public function __construct(Lol $API)
+    {
+        $this->API = $API->getApi();
+    }
+
+    /**
+     * Get the game result between teamA and teamB.
+     * Throws an exception if no game was found.
+     * @param  Team          $teamA Teams can have more players than 5.     
+     * @param  Team          $teamB     
+     * @param  phpTimestamp  $dateLimit Games before this timestamp will not be analyzed
+     *                                  Ideally, that should be the pvpNetUrl generation date
+     * @return true if teamA won, false otherwise.
+     */
+    public function getGameResult(Team $teamA, Team $teamB, $dateLimit = null)
+    {   
+
+        if($dateLimit === null)
+            $dateLimit = time() - 3600 * 24; //One day
+
+        foreach($teamA->getPlayers()->toArray() as $summoner)
+        {   
+            $games = $this->API->game()->recent($summoner->getLolId());
+            foreach($games as $game) 
+            {
+
+                if($game->invalid) continue;
+                if(intval($game->createDate / 1000) < $dateLimit) continue;
+
+                $teamACode = intval($game->stats->team);
+                $teamBCode = ($teamACode === 100 ? 200 : 100);
+                $winner    = $game->stats->win;
+
+                $teamAChecked = 1;
+                $teamBChecked = 0;
+
+                foreach($game->fellowPlayers as $player) {
+                    if($player->teamId === $teamACode && $this->isSummonerInTeam($player->summonerId, $teamA))
+                        $teamAChecked++;
+                    elseif($player->teamId === $teamBCode && $this->isSummonerInTeam($player->summonerId, $teamB))
+                        $teamBChecked++;
+                }
+
+                if($teamAChecked === 5 && $teamBChecked === 5)
+                    return $winner; //We have found the right game, and it's correct :)
+
+            }
+
+        }
+
+        // Not found
+        throw new \Exception("Aucune partie ne peut être validée. Avez vous bien joué la partie ?");
+    }
 
     /**
      * Generate a PVPNet url to join or create a custom game (LoL)
@@ -40,6 +98,15 @@ class PvpNet
         $url_format = "pvpnet://lol/customgame/joinorcreate/%s/%s/%s/%s/%s";
         return sprintf($url_format, $map, $type, $size, $spec, $json_base64);
 
+    }
+
+    private function isSummonerInTeam($summonerId, Team $team)
+    {
+        foreach($team->getPlayers()->toArray() as $player) {
+            if($player->getLolId() === $summonerId) return true;
+        }
+
+        return false;
     }
 
 }
