7238e277ec5d6def3d15d08462554ffe1333ff1c
Quentin Dufour <quentin@dufour.tk>
2014-12-20 06:21:31 +0100

    Finally solve bug of digital payment

--- src/InsaLan/TournamentBundle/Controller/UserController.php
+++ src/InsaLan/TournamentBundle/Controller/UserController.php
@@ -206,6 +206,16 @@ class UserController extends Controller
         $order['PAYMENTREQUEST_0_CURRENCYCODE'] = $tournament->getCurrency();
         $order['PAYMENTREQUEST_0_AMT'] = $price;
         
+        $order['L_PAYMENTREQUEST_0_NAME0'] = 'Place pour le tournoi '.$tournament->getName();
+        $order['L_PAYMENTREQUEST_0_AMT0'] = $tournament->getWebPrice();
+        $order['L_PAYMENTREQUEST_0_DESC0'] = $tournament->getDescription();
+        $order['L_PAYMENTREQUEST_0_NUMBER0'] = 1;
+        
+        $order['L_PAYMENTREQUEST_0_NAME1'] = 'Majoration paiement en ligne';
+        $order['L_PAYMENTREQUEST_0_AMT1'] = $tournament->getOnlineIncreaseInPrice();
+        $order['L_PAYMENTREQUEST_0_DESC1'] = 'Frais de gestion du paiement';
+        $order['L_PAYMENTREQUEST_0_NUMBER1'] = 1;
+
         $storage->updateModel($order);
 
         $payment = $this->get('payum')->getPayment('paypal_express_checkout_and_doctrine_orm');
