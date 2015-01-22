d863c69c28f3e7859659689eaeb4735f91d9c614
Lesterpig <contact@lesterpig.com>
2015-01-22 21:27:41 +0100

    [Migration]

---
+++ app/DoctrineMigrations/Version20150122212341.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150122212341 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE intra_Participant ADD placement INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE intra_Tournament ADD shortName VARCHAR(10) DEFAULT NULL, ADD placement TINYINT(1) NOT NULL');
+        $this->addSql('UPDATE intra_Tournament SET placement = 0');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE intra_Participant DROP placement');
+        $this->addSql('ALTER TABLE intra_Tournament DROP shortName, DROP placement');
+    }
+}
