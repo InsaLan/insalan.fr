54bbf3e3024a4d063225d1a1b0909d638998a5fc
Quentin Dufour <quentin@dufour.tk>
2015-01-26 00:17:41 +0100

    [User] 2nd Hotfix, missing files

---
+++ src/InsaLan/UserBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace InsaLan\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', null, array('required' => true, 'label' => 'Prénom*'))
+            ->add('lastname', null, array('required' => true, 'label' => 'Nom*'))
+            ->add('phoneNumber', null, array('required' => false, 'label' => 'Portable'))
+            ->add('birthdate', 'birthday', array('required' => true, 'label' => 'Date de naissance*'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'InsaLan\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'insalan_userbundle_user';
+    }
+}
