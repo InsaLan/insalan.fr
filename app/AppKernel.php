01e911dc69effeecc0ad411780b534f55fd9d469
Maximilien Richer <youngyukari@gmail.com>
2015-11-10 00:58:32 +0100

    [Debug] Add useful dump() function

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -52,6 +52,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
         }
 
         return $bundles;
