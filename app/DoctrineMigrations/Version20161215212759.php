b5940fe19c905af2d5c5168a7b740e2bad3957e9
SamuelFelton <samuel.felton@etu.unilim.fr>
2016-12-15 22:53:03 +0100

    added migration version

---
+++ app/DoctrineMigrations/Version20161215212759.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161215212759 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE intra_user ADD steamId VARCHAR(255) DEFAULT NULL, ADD battleTag VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE intra_tournament ADD loginType VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE intra_Tournament DROP loginType');
+        $this->addSql('ALTER TABLE intra_User DROP steamId, DROP battleTag');
+    }
+}
