32ab7ce55526b4f438e26ff2b22f5830abe61d76
AntoineBvn <antoine.bou35@gmail.com>
2018-02-09 10:54:34 +0100

    Add Sonata for picture and stream upload in Archives

---
+++ src/InsaLan/ArchivesBundle/Admin/StreamAdmin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace InsaLan\ArchivesBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class StreamAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('url')
+            ->add('date')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('url')
+            ->add('date')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('url')
+            ->add('date')
+        ;
+    }
+    
+    public function prePersist($e) {
+        $e->upload();
+    }
+    
+    public function preUpdate($e) {
+        $this->prePersist($e);
+    }
+}
