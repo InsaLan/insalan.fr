9f43cb03b5874271ec1ae3395037ebeb60b66173
Antoine Bourven <antoine.bou35@gmail.com>
2019-09-11 11:14:35 +0200

    ETicket api: add login and logout

--- src/InsaLan/ApiBundle/Controller/TicketingController.php
+++ src/InsaLan/ApiBundle/Controller/TicketingController.php
@@ -4,11 +4,14 @@ namespace InsaLan\ApiBundle\Controller;
 
 use InsaLan\ApiBundle\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 use InsaLan\TicketingBundle\Entity\ETicket;
 
@@ -21,14 +24,36 @@ class TicketingController extends Controller
     const ERR_TICKET_CANCELLED = array("no" => 4, "msg" => "Ticket cancelled");
 
     /**
+     * @Route("/ticket/login")
+     * @Method({"POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function loginAction(Request $request)
+    {
+      return new Response("OK", Response::HTTP_OK);
+    }
+
+
+    /**
+     * @Route("/ticket/logout")
+     * @Method({"POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function logoutAction(Request $request)
+    {
+      $this->get('security.token_storage')->setToken(null); 
+      $this->get('session')->invalidate();
+      return new Response("OK", Response::HTTP_OK);
+    }
+
+    /**
      * @Route("/ticket/get")
      * @Method({"POST"})
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function getETicketAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        // Deny access if the user is not an admin
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Access denied!');
 
         // Get JSON data
         $parametersAsArray = [];
@@ -41,6 +66,10 @@ class TicketingController extends Controller
         $eTicket = $em->getRepository('InsaLanTicketingBundle:ETicket')->findOneByToken($token);
         if ($eTicket === null) {
           return new JsonResponse(array("err" => self::ERR_TICKET_NOT_FOUND));
+        }
+
+        if ($eTicket->getStatus() === ETicket::STATUS_CANCELLED) {
+          return new JsonResponse(array("err" => self::ERR_TICKET_CANCELLED));
         }
 
         // Find participant
@@ -65,13 +94,12 @@ class TicketingController extends Controller
 
     /**
      * @Route("/ticket/validate")
+     * @Security("has_role('ROLE_ADMIN')")
      * @Method({"POST"})
      */
     public function validateTicketAction(Request $request)
     {
       $em = $this->getDoctrine()->getManager();
-      // Deny access if the user is not an admin
-      $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Access denied!');
 
       // Get JSON data
       $parametersAsArray = [];
