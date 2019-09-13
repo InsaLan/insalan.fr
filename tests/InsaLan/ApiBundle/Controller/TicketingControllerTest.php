2847a03908e8ef01c2ebdd55265758043c2f4ab5
Antoine Bourven <antoine.bou35@gmail.com>
2019-09-13 19:46:04 +0200

    Eticket api: functional tests of login/logout

---
+++ tests/InsaLan/ApiBundle/Controller/TicketingControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\InsaLan\ApiBundle\Controller;
+
+use InsaLan\ApiBundle\Http\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+
+use InsaLan\TicketingBundle\Entity\ETicket;
+
+class TicketingControllerTest extends WebTestCase
+{
+  private $client = null;
+
+  public function setUp()
+  {
+      $this->client = static::createClient();
+  }
+
+  public function testLoginSuccess()
+  {
+    $this->client->request('POST', '/api/ticket/login', [], [], [
+      'PHP_AUTH_USER' => 'admin',
+      'PHP_AUTH_PW'   => 'admin',
+      ]);
+    $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+  }
+
+  public function testLoginFailBadCredentials()
+  {
+    $this->client->request('POST', '/api/ticket/login', [], [], [
+      'PHP_AUTH_USER' => 'admin',
+      'PHP_AUTH_PW'   => 'passwd',
+      ]);
+    $this->assertSame(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode());
+  }
+
+  public function testLoginFailNotAdmin()
+  {
+    $this->client->request('POST', '/api/ticket/login', [], [], [
+      'PHP_AUTH_USER' => 'user',
+      'PHP_AUTH_PW'   => 'user',
+      ]);
+    $this->assertSame(Response::HTTP_FORBIDDEN, $this->client->getResponse()->getStatusCode());
+  }
+
+  public function testLogoutResponse()
+  {
+    // TODO: Find a way to mock login
+    $this->client->request('POST', '/api/ticket/login', [], [], [
+      'PHP_AUTH_USER' => 'admin',
+      'PHP_AUTH_PW'   => 'admin',
+      ]);
+    $this->client->request('POST', '/api/ticket/logout');
+    $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+  }
+
+  public function testLogoutCookieDestruction()
+  {
+    // TODO: Find a way to mock login
+    $this->client->request('POST', '/api/ticket/login', [], [], [
+      'PHP_AUTH_USER' => 'admin',
+      'PHP_AUTH_PW'   => 'admin',
+      ]);
+    $this->client->request('POST', '/api/ticket/logout');
+    $this->client->request('POST', '/api/ticket/logout');
+    $this->assertSame(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode());
+  }
+
+}
