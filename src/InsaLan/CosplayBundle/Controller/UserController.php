3f0d64638b46591ffae4c4e944b8910b2f83b492
Ame04 <camus.armand@gmail.com>
2019-11-14 20:15:59 +0100

    change of the CosplayBundle's userController to create the form and the Cosplayer entity and call the register.html.twig page

--- src/InsaLan/CosplayBundle/Controller/UserController.php
+++ src/InsaLan/CosplayBundle/Controller/UserController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use InsaLan\CosplayBundle\Entity\Cosplayer;
+use InsaLan\CosplayBundle\Form\CosplayerType;
+
 class UserController extends Controller
 {
     /**
@@ -14,6 +17,17 @@ class UserController extends Controller
      */
     public function registerAction()
     {
-        return $this->render('InsaLanCosplayBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $p1 = new Cosplayer();
+        $form = $this->createForm(CosplayerType::class, $p1);
+        if ($form->isSubmitted() && $form->isValid()) {
+        	$em->persist($p1);
+        	$em->flush();
+        	return $this->redirect($this->generateUrl('homepage'));
+        }
+        return $this->render('InsaLanCosplayBundle:User:register.html.twig', array(
+            'form' => $form->createView(),
+            ));
+
     }
 }
