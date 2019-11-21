8f162a0f24978434168073ecfcdad99fca0a9ff6
Ame04 <camus.armand@gmail.com>
2019-11-21 18:35:10 +0100

    created the groupeRegister page and added a little fonction for it in the controller but not working correctly yet

--- src/InsaLan/CosplayBundle/Controller/UserController.php
+++ src/InsaLan/CosplayBundle/Controller/UserController.php
@@ -6,9 +6,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 use InsaLan\CosplayBundle\Entity\Cosplayer;
 use InsaLan\CosplayBundle\Form\CosplayerType;
+use InsaLan\CosplayBundle\Entity\Cosplay;
+use InsaLan\CosplayBundle\Form\CosplayType;
 
 class UserController extends Controller
 {
@@ -25,11 +28,32 @@ class UserController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
         	$em->persist($p1);
         	$em->flush();
-        	return $this->redirect($this->generateUrl('homepage'));
+        	return $this->redirect($this->generateUrl('insalan_cosplay_register'));
         }
         return $this->render('InsaLanCosplayBundle:User:register.html.twig', array(
             'form' => $form->createView(),
             ));
 
+    }
+
+    /**
+     * @Route("/cosplay/groupeRegister")
+     * @Template()
+     */
+    public function groupeRegisterAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $p1 = new Cosplay();
+        $count = new IntegerType;
+        $form = $this->createForm(CosplayType::class, $p1);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        	$em->persist($p1);
+        	$em->flush();
+        	return $this->redirect($this->generateUrl('insalan_cosplay_index'));
+        }
+        return $this->render('InsaLanCosplayBundle:User:groupeRegister.html.twig', array(
+            'form' => $form->createView(),
+            ));
     }
 }
