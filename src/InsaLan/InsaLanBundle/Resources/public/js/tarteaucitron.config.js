4e749d98ca726d9ac92066c56ea76719fdb9585d
Antoine Bourven <antoine.bou35@gmail.com>
2020-02-18 10:39:17 +0100

    Configure cookies script

--- src/InsaLan/InsaLanBundle/Resources/public/js/tarteaucitron.config.js
+++ src/InsaLan/InsaLanBundle/Resources/public/js/tarteaucitron.config.js
@@ -1,18 +1,18 @@
 
 tarteaucitron.init({
-  "privacyUrl": "", /* Privacy policy url */
+  "privacyUrl": "/infos/cookiespolicy", /* Privacy policy url */
 
   "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
   "cookieName": "tarteaucitron", /* Cookie name */
 
   "orientation": "top", /* Banner position (top - bottom) */
-  "showAlertSmall": true, /* Show the small banner on bottom right */
+  "showAlertSmall": false, /* Show the small banner on bottom right */
   "cookieslist": true, /* Show the cookie list */
 
   "adblocker": false, /* Show a Warning if an adblocker is detected */
   "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
   "highPrivacy": true, /* Disable auto consent */
-  "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+  "handleBrowserDNTRequest": true, /* If Do Not Track == 1, disallow all */
 
   "removeCredit": false, /* Remove credit link */
   "moreInfoLink": true, /* Show more info link */
